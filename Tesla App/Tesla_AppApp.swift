6a9b5fbce7953cc11cdeefda459f463f99770c7f
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-29 08:49:32 +0300

    first commit

---
+++ Tesla App/Tesla_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tesla_AppApp.swift
+//  Tesla App
+//
+//  Created by Samuel Baraka on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tesla_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
