afe03a2dbedecfa3de4c6e1d42717afa9862c085
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-31 13:07:58 +0300

    Added colors file

---
+++ Tesla App/Theming/color.swift
@@ -0,0 +1,12 @@
+//
+//  color.swift
+//  Tesla App
+//
+//  Created by Samuel Baraka on 31/08/2022.
+//
+
+import Foundation
+
+extension Color {
+  static let defaultBackground = Color(light: .white, dark: .black)
+}
