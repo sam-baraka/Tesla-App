aa2dcf2cc5cc349941d297e07fd930c7ea626229
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-31 13:08:18 +0300

    Added image and spacing around the image

--- Tesla App/Views/Home/HomePage.swift
+++ Tesla App/Views/Home/HomePage.swift
@@ -11,7 +11,12 @@ struct HomePage: View {
     var body: some View {
         VStack{
             TitleBar()
+            Spacer()
+                    .frame(height: 26)
+            Image("model3").frame(width: 314 )
         }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+        Spacer()
+            .frame(height: 28.5)
     }
 }
 
