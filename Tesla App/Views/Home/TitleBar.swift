2f698ea2cf8f7fcfea5cce9b86b90b5079ac2b0a
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-31 12:48:26 +0300

    Created simple title bar for the home page

---
+++ Tesla App/Views/Home/TitleBar.swift
@@ -0,0 +1,33 @@
+//
+//  TitleBar.swift
+//  Tesla App
+//
+//  Created by Samuel Baraka on 31/08/2022.
+//
+
+import SwiftUI
+
+struct TitleBar: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading){
+                Text("Tesla")
+                HStack {
+                    Image(systemName: "battery.75")
+                    Text("187 KM")
+                }
+            }
+            Spacer()
+            Image(systemName: "person").frame(width: 50, height: 50).clipShape(Circle())
+                .shadow(radius: 10)
+                .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+            
+        }.padding()
+    }
+}
+
+struct TitleBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleBar().previewLayout(.fixed(width: 300, height: 70))
+    }
+}
