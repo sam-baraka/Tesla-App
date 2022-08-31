8074f0a06ab43d39e8055a71e49ff4e9ab772386
Samuel Baraka <sababuvercetti@gmail.com>
2022-08-31 14:08:09 +0300

    Removed view color scheme

---
+++ Tesla App/Views/Home/MiddleTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MiddleTabView.swift
+//  Tesla App
+//
+//  Created by Samuel Baraka on 31/08/2022.
+//
+
+import SwiftUI
+
+struct MiddleTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MiddleTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MiddleTabView()
+    }
+}
